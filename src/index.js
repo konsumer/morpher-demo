268edcfdd3b02eae79ca9cdcd584a9bc183bfd8b
David Konsumer <konsumer@jetboystudio.com>
2016-03-17 00:33:41 -0700

    added a EmelioSheen component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import {render} from 'react-dom'
+import EmilioSheen from './EmilioSheen'
 
-import Home from './Home'
 
-React.render(
-  <Home />,
+class Application extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      sheenness: 0
+    }
+  }
+
+  handleValueChange(ev) {
+    this.setState({
+      sheenness: ev.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div className='Application'>
+        <EmilioSheen sheenness={this.state.sheenness} />
+        <input value={this.state.sheenness} type='range' min={0} max={1} step={0.01} onChange={this.handleValueChange.bind(this)} />
+      </div>
+    )
+  }
+}
+
+render(
+  <Application />,
   document.getElementById('app')
 )
